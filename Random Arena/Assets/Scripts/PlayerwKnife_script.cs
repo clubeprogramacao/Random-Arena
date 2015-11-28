1f4d102c5c031ba3847911b05ea24bbb4c4ad9dc
Erbarbar <filipelopez3@gmail.com>
2015-11-28 03:01:09 +0000

    knife working

--- Random Arena/Assets/Scripts/PlayerwKnife_script.cs
+++ Random Arena/Assets/Scripts/PlayerwKnife_script.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class PlayerwKnife_script : MonoBehaviour {
 	
+	public Collider2D knife_north;
 	public Collider2D knife_east;
+	public Collider2D knife_west;
+	public Collider2D knife_south;
 	public GameObject player;
 
 	private float attackCooldown;
@@ -11,22 +14,25 @@ public class PlayerwKnife_script : MonoBehaviour {
 	public bool hasKnife;
 	public bool attacking;
 	public int facing; // up 8 | down 2 | left 4 | right 6 //
+	public float x;
+	public float y;
 
 	void Awake()
 	{
 		hasKnife = true;
 		attacking = false;
-		knife_east.enabled = false; //
+		facing = 2;
+		updateKnife(facing,false);
 		attackTimer = 0f;
 		attackCooldown = 0.3f;
-		facing = 6;
 	}
 
 	void Update()
 	{
-		if(Input.GetKeyDown("space") && attackTimer == 0 && facing == 6){
+		getDirection ();
+		if(Input.GetKeyDown("space") && attackTimer == 0){
 			attacking = true;
-			knife_east.enabled = true;  //
+			updateKnife(facing,true);
 			attackTimer = attackCooldown;
 		}
 		if (attacking & attackTimer > 0) {
@@ -36,19 +42,50 @@ public class PlayerwKnife_script : MonoBehaviour {
 		}
 		else{
 			attacking = false;
-			knife_east.enabled = false; //
+			updateKnife(facing,false);
 		}
 	}
-	/*
+
 	int getDirection(){
-		float x = player.GetComponent<Player_script> ().Speed_X;
-		float y = player.GetComponent<Player_script> ().Speed_Y;
+		x = player.GetComponent<Player_script> ().Speed_X;
+		y = player.GetComponent<Player_script> ().Speed_Y;
 
-		if( x > y && x > 0){
 
+		if (Mathf.Abs(x) > Mathf.Abs(y)){
+			if (x > 0)
+				facing = 6;
+			if(x < 0)
+				facing = 4;
 		}
-		return 6;
-	}*/
+		if (Mathf.Abs(x) < Mathf.Abs (y)) {
+			if (y > 0)
+				facing = 8;
+			if (y < 0)
+				facing = 2;
+		}
+		return facing;
+	}
+
+	void updateKnife(int dir, bool permission){
+		knife_north.enabled = false;  //
+		knife_east.enabled = false;
+		knife_west.enabled = false;
+		knife_south.enabled = false;
+		switch (dir) {
+		case 2:
+			knife_south.enabled = permission;
+			return;
+		case 4:
+			knife_west.enabled = permission;
+			return;
+		case 6:
+			knife_east.enabled = permission;
+			return;
+		case 8:
+			knife_north.enabled = permission;
+			return;
+		}
+	}
 
 
 }
